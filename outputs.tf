02d7fd4d5fe7c3c2d2eaa4f90263fca683a6e971
Satoshi I <8ato2h1@gmail.com>
2025-05-19 23:55:04 +0000

    Create instance

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-# output "instance_ami" {
-#   value = aws_instance.ubuntu.ami
-# }
+output "instance_ami" {
+  value = aws_instance.ubuntu.ami
+}
 
-# output "instance_arn" {
-#   value = aws_instance.ubuntu.arn
-# }
+output "instance_arn" {
+  value = aws_instance.ubuntu.arn
+}
